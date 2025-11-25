7beff34562abf55d8f6b93ab3e881243ff6c995c
SomSamnang <161201495+SomSamnang@users.noreply.github.com>
2025-11-25 21:51:12 +0700

    coffee_shopping new day  update

--- employees/employee_list.php
+++ employees/employee_list.php
@@ -59,7 +59,8 @@ $stmt->close();
 <title>Employee Lists</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<link rel="stylesheet" href="../css/employee_list.css">
+<link rel="stylesheet" href="/coffee_shops/css/employee_list.css">
+
 </head>
 <body>
 
